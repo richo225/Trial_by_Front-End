76272a55666b4d7ca853d555e8be4acb85af8db9
Richard Bates <richo225@gmail.com>
2016-10-28 07:42:00 +0100

    Add a bit of JS with reveal plugin for quote

---
+++ test/js/main.js
@@ -0,0 +1,17 @@
+jQuery(function () {
+// reveals quote after a delay
+  var options2 = {
+    id: 'quote-text'
+    , delay: 3000
+    , background: '#e7e7e7'
+  };
+  $('.quote-text').initReveal(options2);
+
+  // var options4 = {
+  //   id: 'bottom-info'
+  //   , auto: false
+  //   , trigger: 'on-visible'
+  // };
+  // $('.bottom-info').initReveal(options4);
+
+});
